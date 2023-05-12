f26af5251d1578fea586c70c284b41f7b0f7c3bb
pramitchoudhary <pramitchoudhary@gmail.com>
2023-05-11 23:10:15 -0700

    Minor prompt design update; added references

--- sidekick/configs/prompt_template.py
+++ sidekick/configs/prompt_template.py
@@ -1,21 +1,27 @@
 TASK_PROMPT = {
     "system_prompt": "Act as a Data Analyst",
     "user_prompt": """
-        ### For Table: {table_name} Given an input question, only return specific tasks as an ordered itemized list for SQL generation that answer the question. Don't generate SQL code.
-        Infer the return type of the question. Add a task to return final output. Tasks:
+        ### For Table: {table_name} Given an input (question), only return specific tasks as an ordered itemized list for SQL generation that answer the question. Don't generate SQL code.
+        Infer the return type of the question. Add a task to return final output.
         # Example data: \n {samples}
-        # question:\n {question_str}
+        # (question):\n {question_str}
+        # Output format: Tasks: list of tasks
     """,
 }
 
+# Few shot learning prompt
+## Task Description
+## Examples
+## Prompt
+# Reference: https://arxiv.org/pdf/2005.14165.pdf
 QUERY_PROMPT = """
                 ### System: Act as a SQL Expert
-                # Given an input question, only generate syntactically correct SQL queries
+                # Given an input (question), only generate syntactically correct SQL queries
                 # Pick the SQL query which has the highest average log probability of explaining the
                 candidate question
                 ### {dialect} SQL tables
                 Examples:\n{_sample_queries}
-                ### question:\n{_question};
+                ### *question*:\n{_question};
                 # SELECT 1
                 ### Tasks:\n{_tasks}
                 ### Suggestions:
@@ -33,7 +39,7 @@ DEBUGGING_PROMPT = {
     "system_prompt": "Act as a SQL expert for PostgreSQL code",
     "user_prompt": """
                 ### Fix syntax errors for provided SQL Query.
-                # Add <start_code> as prefix and <end_code> as suffix to generated SQL
+                # Add ``` as prefix and ``` as suffix to generated SQL
                 # Error: {ex_traceback}
                 Query:\n {qry_txt}
                 """,
