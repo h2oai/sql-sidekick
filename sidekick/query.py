e08a0faafd32411abc1ba2827036ce9a4ffff812
pramitchoudhary <pramitchoudhary@gmail.com>
2023-05-25 16:22:53 -0700

    Use all-MiniLM-L6-v1 embeddings for fast duplicate removal

--- sidekick/query.py
+++ sidekick/query.py
@@ -3,15 +3,18 @@ import os
 import sys
 from pathlib import Path
 
+import numpy as np
 import openai
 import sqlglot
 from configs.prompt_template import DEBUGGING_PROMPT, QUERY_PROMPT, TASK_PROMPT
 from examples.sample_data import sample_values, samples_queries
 from langchain import OpenAI
-from llama_index import GPTSimpleVectorIndex, GPTSQLStructStoreIndex, LLMPredictor, ServiceContext, SQLDatabase
+from llama_index import (GPTSimpleVectorIndex, GPTSQLStructStoreIndex,
+                         LLMPredictor, ServiceContext, SQLDatabase)
 from llama_index.indices.struct_store import SQLContextContainerBuilder
 from loguru import logger
 from sqlalchemy import create_engine
+from utils import remove_duplicates
 
 
 class SQLGenerator:
@@ -23,6 +26,7 @@ class SQLGenerator:
         self.path = path
         self._tasks = None
         self.openai_key = openai_key
+        self.content_queries = None
 
     def build_index(self, persist: bool = True):
         # Below re-assignment of the OPENAI API key is weird but without that, it throws an error.
@@ -38,18 +42,21 @@ class SQLGenerator:
 
     def update_context_queries(self):
         # Read the history file and update the context queries
-        with open(f"{self.path}/var/lib/tmp/data/history.jsonl", "r") as in_file:
-            for line in in_file:
-                # Format:
-                # """
-                # # query:
-                # # answer:
-                # """
-                query = json.loads(line)["Query"]
-                response = json.loads(line)["Answer"]
-                _new_samples = f"""# query: {query}\n# answer: {response}"""
-                _combined = [samples_queries, _new_samples]
-                new_context_queries = "\n".join(_combined)
+        new_context_queries = samples_queries
+        history_file = f"{self.path}/var/lib/tmp/data/history.jsonl"
+        if Path(history_file).exists():
+            with open(f"{self.path}/var/lib/tmp/data/history.jsonl", "r") as in_file:
+                for line in in_file:
+                    # Format:
+                    # """
+                    # # query:
+                    # # answer:
+                    # """
+                    query = json.loads(line)["Query"]
+                    response = json.loads(line)["Answer"]
+                    _new_samples = f"""# query: {query}\n# answer: {response}"""
+                    new_context_queries.append(_new_samples)
+
         return new_context_queries
 
     def _query_tasks(self, question_str, data_info, sample_queries, table_name: list):
@@ -97,11 +104,11 @@ class SQLGenerator:
                     system_prompt = DEBUGGING_PROMPT["system_prompt"]
                     user_prompt = DEBUGGING_PROMPT["user_prompt"].format(ex_traceback=ex_traceback, qry_txt=qry_txt)
                     # Role and content
-                    query_txt = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
+                    query_msg = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
 
                     completion = openai.ChatCompletion.create(
                         model="gpt-3.5-turbo-0301",
-                        messages=query_txt,
+                        messages=query_msg,
                     )
                     res = completion.choices[0].message["content"]
                     if "SELECT" not in res:
@@ -116,12 +123,16 @@ class SQLGenerator:
         try:
             # Step 1: Given a question, generate tasks to possibly answer the question and persist the result -> tasks.txt
             # Step 2: Append task list to 'query_prompt_template', generate SQL code to answer the question and persist the result -> sql.txt
-            context_queries = self.update_context_queries()
+            context_queries: list = self.update_context_queries()
 
             # Remove duplicates from the context queries
-            generate_sentence_embeddings(context_queries)
+            m_path = f"{self.path}/models/sentence_transformer/all-MiniLM-L6-v1/"
+            duplicates_idx = remove_duplicates(context_queries, m_path)
+            updated_context = np.delete(np.array(context_queries), duplicates_idx).tolist()
 
-            task_list = self._query_tasks(input_question, sample_values, context_queries, table_name)
+            _queries = "\n".join(updated_context)
+            self.content_queries = _queries
+            task_list = self._query_tasks(input_question, sample_values, _queries.lower(), table_name)
             with open(f"{self.path}/var/lib/tmp/data/tasks.txt", "w") as f:
                 f.write(task_list)
             return task_list
@@ -134,7 +145,11 @@ class SQLGenerator:
         additional_context = json.load(open(context_file, "r")) if Path(context_file).exists() else {}
 
         # Attempt updating in-case additional context is provided
-        context_queries = self.update_context_queries()
+        context_queries = None
+        if self.content_queries:
+            context_queries = self.content_queries
+        else:
+            context_queries = self.update_context_queries()
         query_str = QUERY_PROMPT.format(
             dialect=_dialect,
             _question=input_question.lower(),
