1b754d7f9bdafad9808535bee6981bbfce16c1a0
pramitchoudhary <pramitchoudhary@gmail.com>
2023-06-06 13:30:42 -0700

    Improve prompt template

--- sidekick/configs/prompt_template.py
+++ sidekick/configs/prompt_template.py
@@ -1,10 +1,11 @@
 TASK_PROMPT = {
     "system_prompt": "Act as a Data Analyst",
     "user_prompt": """
-        ### Given an input *Question*, only return specific and informative tasks as an ordered numeric list for SQL generation that answer the question.
-        Use the *History* and *Context* section for co-reference and to infer relationships.
+        ### Given an input *Question*, let's work it out in a detailed step by step way and only return specific and informative tasks as an ordered numeric list for SQL generation to be sure we have the right answer.
+        Use the *History* and *Context* section for co-reference and to infer relationships. *Context* contains entity mapping containing keys:values.
         If the words in the *Question* do not match column names *Data* section; Search for them in *Context* section.
         Always use *Context* with highest similarity score with the *Question*.
+        If words in the *Question* match more than one key, include both the values using "or" when forming step by step tasks.
         If no information related to the *Question* is found; attempt to predict and reason for possible tasks.
         Infer the return type of the Question. Do not generate final complete SQL response, only return tasks.
         # *Data:* \nFor table {_table_name} schema info is mentioned below,\n{_data_info}
@@ -21,7 +22,8 @@ TASK_PROMPT = {
 # Reference: https://arxiv.org/pdf/2005.14165.pdf
 QUERY_PROMPT = """
                 ### System: Act as a SQL Expert
-                # Given an input *Question*, only generate syntactically correct SQL queries using step by step reasoning from *Tasks* section.
+                # Given an input *Question*, only generate syntactically correct SQL queries.
+                # Let's work it out in a detailed step by step way using the reasoning from *Tasks* section.
                 # Pick the SQL query which has the highest average log probability of explaining the
                 candidate *Question*.
                 ### {dialect} SQL tables
